fc84caafa87af0920e8109168b58593e01e92e64
Sergey Edalov <edalov94@gmail.com>
2024-02-25 15:24:27 +0300

    Hm_1

---
+++ Homeworks/Homework_1/Cl_Homework_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Sockets;
+
+namespace Cl_Homework_1
+{
+    public class Client
+    {
+        static async void Main(string[] args)
+        {
+            using TcpClient tcpClient = new TcpClient();
+            Console.Write("Введите свое имя: ");
+            string? userName = Console.ReadLine();
+            Console.WriteLine($"Добро пожаловать, {userName}");
+            Thread.Sleep(1000);
+            try
+            {
+                tcpClient.Connect("127.0.0.1", 12345);
+
+                Console.WriteLine("Подключен к чату");
+
+                var reader = new StreamReader(tcpClient.GetStream());
+                var writer = new StreamWriter(tcpClient.GetStream());
+                if (writer is null || reader is null) return;
+                Task.Run(() => ReceiveMessage(reader));
+                await SendMessage(writer);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+
+            async Task SendMessage(StreamWriter writer)
+            {
+                // сначала отправляем имя
+                await writer.WriteLineAsync(userName);
+                await writer.FlushAsync();
+                Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+
+                while (true)
+                {
+                    string? message = Console.ReadLine();
+                    await writer.WriteLineAsync(message);
+                    await writer.FlushAsync();
+                }
+            }
+
+            async Task ReceiveMessage(StreamReader reader)
+            {
+                while (true)
+                {
+                    string? message = await reader.ReadLineAsync();
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/Homework_1/Ser_Homework_1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Ser_Homework_1
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new ChatServer();
+            await server.Run();
+        }
+    }
+
+
+    public class ChatServer
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 12345);
+        List<TcpClient> clients = new List<TcpClient>();
+
+        public async Task Run()
+        {
+            try
+            {
+                listener.Start();
+                Console.Out.WriteLine("Сервер запущен");
+
+                while (true)
+                {
+                    var tcpClient = await listener.AcceptTcpClientAsync();
+
+                    Console.WriteLine("Успешно подключен");
+
+                    clients.Add(tcpClient);
+
+                    Task.Run(() => ProcessClient(tcpClient, clients));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync(ex.Message.ToString());
+            }
+        }
+
+        public async Task ProcessClient(TcpClient tcpClient, List<TcpClient> clients)
+        {
+            try
+            {
+                var reader = new StreamReader(tcpClient.GetStream());
+                //string? userName = await reader.ReadLineAsync();
+                //string? message = $"{userName} вошел в чат";
+                string? message = await reader.ReadLineAsync();
+                // посылаем сообщение о входе в чат всем подключенным пользователям
+                foreach (var client in clients)
+                {
+                    var writer = new StreamWriter(client.GetStream());
+                    await writer.WriteLineAsync(message);
+                    await writer.FlushAsync();
+                }
+                Console.WriteLine(message);
+
+                while (true)
+                {
+                    //var reader = new StreamReader(tcpClient.GetStream());
+
+                    message = await reader.ReadLineAsync();
+
+                    Console.WriteLine($"{message}");
+                    Console.WriteLine("*****");
+
+                    foreach (var client in clients)
+                    {
+                        var writer = new StreamWriter(client.GetStream());
+                        await writer.WriteLineAsync(message);
+                        await writer.FlushAsync();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+        }
+    }
+}

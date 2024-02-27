e9e5c7722f0182e1bc83e7aacceb669510d739d7
Sergey Edalov <edalov94@gmail.com>
2024-02-27 17:46:11 +0300

    Homework_2

--- Homeworks/Homework_2/Cl_Homework_2/Program.cs
+++ Homeworks/Homework_2/Cl_Homework_2/Program.cs
@@ -1,10 +1,66 @@
-﻿namespace Cl_Homework_2
+﻿using System.Net.Sockets;
+
+namespace Cl_Homework_2
 {
-    internal class Program
+
+    //Добавьте возможность ввести слово Exit в чате клиента, чтобы можно было завершить его работу.
+    //В коде сервера добавьте ожидание нажатия клавиши, чтобы также прекратить его работу.
+    public class Client
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using TcpClient tcpClient = new TcpClient();
+            Console.Write("Введите свое имя: ");
+            string? userName = Console.ReadLine();
+            Thread.Sleep(1000);
+
+            try
+            {
+                tcpClient.Connect("127.0.0.1", 12345);
+                Console.WriteLine($"Добро пожаловать, {userName}");
+
+                var reader = new StreamReader(tcpClient.GetStream());
+                var writer = new StreamWriter(tcpClient.GetStream());
+
+                if (writer is null || reader is null) return;
+
+                Task.Run(() => ReceiveMessage(reader));
+                await SendMessage(writer);
+
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+            finally { tcpClient?.Close(); }
+
+            async Task SendMessage(StreamWriter writer)
+            {
+                await writer.WriteLineAsync(userName);
+                await writer.FlushAsync();
+
+                Console.WriteLine("Для отправки сообщений введите сообщение и нажмите Enter");
+                Console.WriteLine("Для выхода из чата введите Exit");
+
+                string? message = "";
+
+                while (message != "exit")
+                {                    
+                    if (tcpClient.Connected)
+                    {
+                        message = Console.ReadLine().ToLower();
+
+                        await writer.WriteLineAsync(message);
+                        await writer.FlushAsync();
+                    }
+                }
+            }
+
+            async Task ReceiveMessage(StreamReader reader)
+            {
+                while (true)
+                {
+                    string? message = await reader.ReadLineAsync();
+                    Console.WriteLine(message);
+                }
+            }
         }
     }
 }
---
+++ Homeworks/Homework_2/Ser_Homework_2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+
+namespace Ser_Homework_2
+{
+    //Добавьте возможность ввести слово Exit в чате клиента, чтобы можно было завершить его работу.
+    //В коде сервера добавьте ожидание нажатия клавиши, чтобы также прекратить его работу.
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var server = new ChatServer();
+            await server.Run();
+        }
+    }
+
+    public class ChatServer
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 12345);
+        List<TcpClient> clients = new List<TcpClient>();
+
+        public async Task Run()
+        {
+            try
+            {
+                listener.Start();
+                Console.Out.WriteLine("Сервер запущен");
+
+                while (true)
+                {
+                    Task.Run(() => ServerOff(clients));
+
+                    var tcpClient = await listener.AcceptTcpClientAsync();
+                    Console.WriteLine("Успешно подключен");
+                    clients.Add(tcpClient);
+                    
+                    Task.Run(() => ProcessClient(tcpClient, clients));
+                }
+            }
+            catch (Exception ex)
+            {
+                await Console.Out.WriteLineAsync(ex.Message.ToString());
+            }
+            finally { listener.Stop(); }
+        }
+
+        private async Task ProcessClient(TcpClient tcpClient, List<TcpClient> clients)
+        {
+            try
+            {
+                var reader = new StreamReader(tcpClient.GetStream());
+
+                string? userName = await reader.ReadLineAsync();
+                string? message = "";
+
+                await Broadcast(clients, $"{userName} вошел в чат");
+
+                Console.WriteLine(message);
+
+                while (true)
+                {
+                    message = await reader.ReadLineAsync();
+
+                    Console.WriteLine($"{userName}: {message}\n*****");
+
+                    if (message == "exit") break;
+
+                    await Broadcast(clients, $"{userName}: {message}");
+                }
+                await Broadcast(clients, $"{userName}: Вышел из чата");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+            finally { DeleteUser(tcpClient, clients); }
+        }
+
+        private void DeleteUser(TcpClient tcpClient, List<TcpClient> clients)
+        {
+            clients.Remove(tcpClient);
+            tcpClient?.Close();
+        }
+
+        private async Task Broadcast(List<TcpClient> clients, string formatMessage)
+        {
+            foreach (var client in clients)
+            {
+                var writer = new StreamWriter(client.GetStream());
+                await writer.WriteLineAsync(formatMessage);
+                await writer.FlushAsync();
+            }
+        }
+
+        private async Task ServerOff(List<TcpClient> clients)
+        {
+            string off = await Console.In.ReadLineAsync();
+
+            if (off == "q")
+            {
+                foreach (var client in clients)
+                {
+                    client.Close();
+                }
+                listener.Stop();
+            }
+        }
+    }
+}
